36c8b4b4aa07cc34c17e0e0c0c4ff173ae90bc1e
Nicole Cordes <typo3@cordes.co>
2019-03-09 23:42:08 +0100

    [TASK] Add basic extension files

---
+++ README.md
@@ -0,0 +1,7 @@
+# TYPO3 Extension t3tags
+
+[![Latest Stable Version](https://img.shields.io/packagist/v/ichhabrecht/t3tags.svg)](https://packagist.org/packages/ichhabrecht/t3tags)
+[![Build Status](https://img.shields.io/travis/IchHabRecht/t3tags/master.svg)](https://travis-ci.org/IchHabRecht/t3tags)
+[![StyleCI](https://styleci.io/repos/138314722/shield?branch=master)](https://styleci.io/repos/138314722)
+
+Generate tag fields for every record type. 
---
+++ ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "t3tags".
+ *
+ * Auto generated 09-03-2019 23:40
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'T3TAGS - TYPO3 Tagging Api',
+  'description' => 'Generate tag fields for every record type',
+  'category' => 'misc',
+  'author' => 'Nicole Cordes',
+  'author_email' => 'typo3@cordes.co',
+  'author_company' => 'biz-design',
+  'state' => 'stable',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'clearCacheOnLoad' => 0,
+  'version' => '0.1.0',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '8.7.0-8.7.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  '_md5_values_when_last_written' => 'a:7:{s:7:"LICENSE";s:4:"b234";s:9:"README.md";s:4:"9348";s:13:"composer.json";s:4:"0d2d";s:13:"composer.lock";s:4:"f9ea";s:16:"phpunit.xml.dist";s:4:"6068";s:24:"sonar-project.properties";s:4:"e6b3";s:36:"Resources/Public/Icons/Extension.svg";s:4:"e822";}',
+);
+
