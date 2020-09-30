234bd24cb94540857f41ab7f7090bd17395df877
Nicole Cordes <typo3@cordes.co>
2020-09-30 13:11:28 +0200

    [RELEASE] Release of t3tags 1.0.1

--- Tests/Functional/Fixtures/Extensions/t3tags_test/ext_emconf.php
+++ Tests/Functional/Fixtures/Extensions/t3tags_test/ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "t3tags_test".
  *
- * Auto generated 14-03-2019 13:55
+ * Auto generated 30-09-2020 12:22
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -33,6 +33,6 @@ $EM_CONF[$_EXTKEY] = array (
     array (
     ),
   ),
-  '_md5_values_when_last_written' => 'a:3:{s:14:"ext_tables.sql";s:4:"f28c";s:42:"Configuration/TCA/Overrides/tt_content.php";s:4:"9abb";s:36:"Resources/Public/Icons/Extension.svg";s:4:"e822";}',
+  '_md5_values_when_last_written' => 'a:3:{s:14:"ext_tables.sql";s:4:"f28c";s:42:"Configuration/TCA/Overrides/tt_content.php";s:4:"85b2";s:36:"Resources/Public/Icons/Extension.svg";s:4:"e822";}',
 );
 
