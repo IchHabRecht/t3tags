7bee60d8e9e43338fea5f1b690bcee3d8422d555
Nicole Cordes <typo3@cordes.co>
2021-06-06 00:48:45 +0200

    [TASK] Update supporting files to EXT:my_skeleton 0.5.3

--- Tests/Functional/Fixtures/Extensions/t3tags_test/ext_emconf.php
+++ Tests/Functional/Fixtures/Extensions/t3tags_test/ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '8.7.8-9.5.99',
+      'typo3' => '10.4.0-10.4.99',
       't3tags' => '0.1.0',
     ),
     'conflicts' => 
